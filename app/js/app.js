1a0f0aa0cd4e267f3d6a512761a3cf4611de336b
Simon Denier <simon@sogilis.com>
2013-08-06 18:02:28 +0200

    Basic setup for mobile/desktop bootstrap + templates

--- app/js/app.js
+++ app/js/app.js
@@ -1,17 +1,34 @@
 'use strict';
 
-angular.module('surgir', ['surgir.search']).
-  config(['$routeProvider', function($routeProvider) {
+angular.module('surgir', ['surgir.search']);
+
+if (window.matchMedia('only screen and (max-device-width:480px)').matches) {
+  angular.module('surgir').config(['$routeProvider', function($routeProvider) {
     $routeProvider.
-      when('/', {templateUrl: 'views/home.html'}).
-      when('/results', {templateUrl: 'views/results.html',
+      when('/', {templateUrl: 'views/mobile/home.html'}).
+      when('/results', {templateUrl: 'views/mobile/results.html',
                         controller: 'ResultsController'}).
-      when('/results/:resultId', {templateUrl: 'views/notice.html',
+      when('/filters', {templateUrl: 'views/mobile/filters.html',
+                        controller: 'FiltersController'}).
+      when('/results/:resultId', {templateUrl: 'views/mobile/notice.html',
                         controller: 'RecordController'}).
-      when('/notice/:permalink', {templateUrl: 'views/notice.html'}).
+      when('/notice/:permalink', {templateUrl: 'views/mobile/notice.html'}).
       otherwise({redirectTo: '/'});
-  }]).
-  run(['Collections', 'CollectionGroupConfig',
+  }]);
+} else {
+  angular.module('surgir').config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+      when('/', {templateUrl: 'views/classic/home.html'}).
+      when('/results', {templateUrl: 'views/classic/results.html',
+                        controller: 'ResultsController'}).
+      when('/results/:resultId', {templateUrl: 'views/classic/notice.html',
+                        controller: 'RecordController'}).
+      when('/notice/:permalink', {templateUrl: 'views/classic/notice.html'}).
+      otherwise({redirectTo: '/'});
+  }]);
+}
+
+angular.module('surgir').run(['Collections', 'CollectionGroupConfig',
   function(Collections, CollectionGroup) {
     Collections.fetch(CollectionGroup);
   }]);
