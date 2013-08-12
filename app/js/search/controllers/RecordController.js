f64efe38d0bcc0201b08a30d31370f20c8c8e7ba
Simon Denier <simon@sogilis.com>
2013-08-12 11:52:18 +0200

    Detailed fields for a record and Basic grid

--- app/js/search/controllers/RecordController.js
+++ app/js/search/controllers/RecordController.js
@@ -3,5 +3,9 @@
 angular.module('surgir.search').controller('RecordController',
   ['$scope', '$routeParams', 'RecordRetriever',
   function($scope, $routeParams, RecordRetriever) {
-    $scope.notice = RecordRetriever.getRecordNotice($routeParams.resultId);
+    $scope.notice = RecordRetriever.getRecordNotice($routeParams.resultId).
+      then(function(notice) {
+        notice.direct_urls = notice.direct_url.split('-_-');
+        return notice;
+      });
   }]);
