10fdad97ccfaa1b52ce4437e5f3aa7a86096157e
Simon Denier <simon@sogilis.com>
2013-08-13 16:16:52 +0200

    POC using html2js to generate templates

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,8 +9,7 @@ module.exports = function(grunt) {
 
     copy: {
       index: { src: 'app/index.prod.html', dest: 'dist/index.html' },
-      assets: {expand: true, cwd: 'app/', src: ['assets/**'], dest: 'dist/'},
-      views: {expand: true, cwd: 'app/', src: ['views/**'], dest: 'dist/'}
+      assets: {expand: true, cwd: 'app/', src: ['assets/**'], dest: 'dist/'}
     },
 
     cssmin: {
@@ -28,6 +27,25 @@ module.exports = function(grunt) {
       }
     },
 
+    html2js: {
+      options: {
+        base: 'app/',
+        module: 'surgir.templates'
+      },
+      surgir: {
+        src: ['app/views/**/*.html'],
+        dest: 'dist/templates/templates.js'
+      },
+      classic: {
+        src: ['app/views/classic/*.html', 'app/views/partials/*.html'],
+        dest: 'dist/templates/classic.js'
+      },
+      mobile: {
+        src: ['app/views/mobile/*.html', 'app/views/partials/*.html'],
+        dest: 'dist/templates/mobile.js'
+      },
+    },
+
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> v<%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
@@ -35,7 +53,7 @@ module.exports = function(grunt) {
       },
       build: {
         files: {
-          'dist/js/<%= pkg.name %>.min.js': ['app/lib/**/*.js', 'app/js/**/*.js']
+          'dist/js/<%= pkg.name %>.min.js': ['app/lib/**/*.js', 'app/js/**/*.js', 'dist/templates/templates.js']
         }
       }
     }
@@ -43,10 +61,11 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-html2js');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.registerTask('default', ['copy', 'cssmin', 'uglify']);
-  grunt.registerTask('release', ['clean', 'copy', 'cssmin', 'uglify']);
+  grunt.registerTask('default', ['copy', 'cssmin', 'html2js', 'uglify']);
+  grunt.registerTask('release', ['clean', 'copy', 'cssmin', 'html2js', 'uglify']);
 
 };
