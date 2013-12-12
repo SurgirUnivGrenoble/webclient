9dc93da921e1acf91de0002bab05262016cfc813
Simon Denier <simon@sogilis.com>
2013-12-12 12:17:45 +0100

    Display 'Chargement' while loading a notice

--- app/js/search/controllers/RecordController.js
+++ app/js/search/controllers/RecordController.js
@@ -4,8 +4,10 @@ angular.module('surgir.search').controller('RecordController',
   ['$scope', '$routeParams', 'RecordRetriever', 'NoticeProcessor',
   function($scope, $routeParams, RecordRetriever, Notice) {
     $scope.backToResults = true;
+    $scope.loading = true;
     $scope.notice = RecordRetriever.getRecordNotice($routeParams.resultId).
       then(function(notice) {
+        $scope.loading = false;
         return Notice.filter(notice);
       });
   }]);
